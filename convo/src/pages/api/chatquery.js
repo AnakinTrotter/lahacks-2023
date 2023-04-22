d4a23aecaefdbc7496f2ecf5b50d6c053ccdf217
Anakin Trotter <anakin.trotter@gmail.com>
2023-04-22 12:26:27 -0700

    Study page passes in questions

---
+++ convo/src/pages/api/chatquery.js
@@ -0,0 +1,41 @@
+// return a few questions delimited by ~
+export default async function handler(req, res) {
+  // const response = await fetch('https://api.gpt4.ai/completion', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Authorization': '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'
+  //   },
+  //   body: JSON.stringify({
+  //     prompt: 'Hello, world!',
+  //     max_tokens: 50
+  //   })
+  // });
+
+  const data = {
+    "id": "chatcmpl-abc123",
+    "object": "chat.completion",
+    "created": 1677858242,
+    "model": "gpt-3.5-turbo-0301",
+    "usage": {
+      "prompt_tokens": 13,
+      "completion_tokens": 7,
+      "total_tokens": 20
+    },
+    "choices": [
+      {
+        "message": {
+          "role": "assistant",
+          "content": "\n\nThis is a question!~Here is another question!"
+        },
+        "finish_reason": "stop",
+        "index": 0
+      }
+    ]
+  }
+  //const data = await response.json();
+
+  const ans = data.choices[0].message.content.trim()
+
+  res.status(200).json({ questions: ans.split('~') });
+}
--- convo/src/pages/study/[uuid].js
+++ convo/src/pages/study/[uuid].js
@@ -1,14 +1,14 @@
 import DisplayPage from "@/components/DisplayPage"
 import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
-
-const questions = ["what is the capital of poland", "i hate school", "a", "b", "c", "d"]
+import { updateQuestions } from "@/database/dbUtils";
 
 export default function Study({ data }) {
     const router = useRouter();
     const { uuid } = router.query;
     const [paragraphData, setParagraphData] = useState([]);
     const [paragraphs, setParagraphs] = useState([]);
+    const [questionsData, setQuestionsData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -18,20 +18,42 @@ export default function Study({ data }) {
                 const paragraphs = data.map((p) => p.original);
                 setParagraphs(paragraphs);
 
-                for (const cur of data) {
+                for (const index in data) {
+                    const cur = data[index];
                     if (!cur.questions) {
+                        const response = await fetch('/api/chatquery', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                paragraph: cur.original
+                            })
+                        });
 
+                        const questions = await response.json();
+
+                        // Update the cur object with questions
+                        cur.questions = questions.questions;
+                        setQuestionsData((prev) => [...prev, cur.questions])
+                        // Update the data array with updated cur object
+                        data[index] = cur;
+                        // Save the updated data to local storage
+                        const sessions = JSON.parse(localStorage.getItem('sessions')) || {};
+                        sessions[uuid] = { values: data };
+                        localStorage.setItem('sessions', JSON.stringify(sessions));
                     }
                 }
                 setParagraphData(data);
             }
+
         };
         fetchData();
     }, [uuid]);
 
-    if (!paragraphData || !paragraphs) {
+    if (!paragraphData || !paragraphs || !questionsData) {
         return <div>Loading...</div>;
     }
 
-    return <DisplayPage uuid={uuid} paragraphData={paragraphData} paragraphs={paragraphs} questions={questions} />;
+    return <DisplayPage uuid={uuid} paragraphData={paragraphData} paragraphs={paragraphs} questions={questionsData} />;
 }
