baa497f81e61622dc52b005cee5b2c68244db2bc
Anakin Trotter <anakin.trotter@gmail.com>
2023-04-22 00:04:08 -0700

    Mastery page done

--- convo/src/pages/test.js
+++ convo/src/pages/test.js
@@ -1,10 +1,19 @@
 import Chat from "@/components/Chat"
 import { Container } from "react-bootstrap"
+import StudyMastery from "./mastery";
 
 const questions = ["what is the capital of poland", "i hate school"]
+const insights = [
+    "Identify your learning style",
+    "Set goals and track your progress",
+    "Focus on comprehension, not memorization",
+    "Take breaks and prioritize self-care",
+    "Ask questions and seek help when needed",
+];
 
 export default function Test() {
     return <Container>
         <Chat questions={questions}/>
+        <StudyMastery percent={40} insights={["hi", "bye"]} />
     </Container>
 }
