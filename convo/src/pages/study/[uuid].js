9a5e8b627d09b1f8e45f95553f432a0df0addeef
Anakin Trotter <anakin.trotter@gmail.com>
2023-04-22 10:37:52 -0700

    Fixed localStorage bug

--- convo/src/pages/study/[uuid].js
+++ convo/src/pages/study/[uuid].js
@@ -1,15 +1,28 @@
 import DisplayPage from "@/components/DisplayPage"
 import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
 
 const questions = ["what is the capital of poland", "i hate school", "a", "b", "c", "d"]
 
-export default function Study() {
+export default function Study({ data }) {
     const router = useRouter();
     const { uuid } = router.query;
-    const paragraphData = JSON.parse(localStorage.getItem('sessions'))[uuid].values
-    const paragraphs = paragraphData.map((p) => p.original)
+    const [paragraphData, setParagraphData] = useState([]);
+    const [paragraphs, setParagraphs] = useState([]);
 
-    // change to use just the one prop
-    return <DisplayPage paragraphData={paragraphData} paragraphs={paragraphs} questions={questions} />
+    useEffect(() => {
+        const localStorageData = localStorage.getItem('sessions');
+        if (localStorageData) {
+            const data = JSON.parse(localStorageData)[uuid]?.values || [];
+            const paragraphs = data.map((p) => p.original);
+            setParagraphData(data);
+            setParagraphs(paragraphs);
+        }
+    }, [uuid]);
 
+    if (!paragraphData || !paragraphs) {
+        return <div>Loading...</div>;
+    }
+
+    return <DisplayPage paragraphData={paragraphData} paragraphs={paragraphs} questions={questions} />;
 }
