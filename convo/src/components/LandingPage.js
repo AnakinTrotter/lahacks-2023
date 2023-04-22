53df4335bd5065d3036fa0d66765d74eddbcebfa
Anakin Trotter <anakin.trotter@gmail.com>
2023-04-22 01:53:17 -0700

    Landing page submit working

--- convo/src/components/LandingPage.js
+++ convo/src/components/LandingPage.js
@@ -8,49 +8,58 @@ import { Form } from 'react-bootstrap';
 import Quiz from '@/components/quiz';
 import InfoText from '@/components/InfoText';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+
 
 
 const LandingPage = () => {
+  const router = useRouter()
 
-    const [textareaValue, setTextareaValue] = useState('');
+  const [textareaValue, setTextareaValue] = useState('');
 
-    const handleTextareaChange = (event) => {
-      setTextareaValue(event.target.value);
-    };
-  
-    const handleFormSubmit = (event) => {
-      event.preventDefault();
-      console.log(textareaValue);
-      // Submit textareaValue to server or perform other actions here
-    };
+  const handleTextareaChange = (event) => {
+    setTextareaValue(event.target.value);
+  };
 
-    
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (!textareaValue || textareaValue === "") {
+      return
+    }
+    console.log(textareaValue);
+    // Submit textareaValue to server or perform other actions here
+    // create db values
+    const uuid = crypto.randomUUID()
+    router.push(`/study/${uuid}`)
+  };
+
+
 
   return (
-    <Container fluid style ={{maxHeight:'100vh'}}>
-    <div className="row" style ={{borderBottom: '2px solid black', position:'absolute', backgroundColor:'white', height:'10vh', width:'100%'}}>
-                <h1>Convo</h1>
-                
-    </div>
-    <div className="row align-items-start">
+    <Container fluid style={{ maxHeight: '100vh' }}>
+      <div className="row" style={{ borderBottom: '2px solid black', position: 'absolute', backgroundColor: 'white', height: '10vh', width: '100%' }}>
+        <h1>Convo</h1>
+
+      </div>
+      <div className="row align-items-start">
         <div className="col" style={{ paddingTop: '10vh', height: '100vh', borderRight: '2px solid black' }}>
-            <div style={{margin:'20px'}}>
-                <InfoText/>
-            </div>
+          <div style={{ margin: '20px' }}>
+            <InfoText />
+          </div>
         </div>
 
         <div className="col" style={{ paddingTop: '10vh', height: '100vh', borderRight: '2px solid black' }}>
-            <div style={{margin:'20px'}}>
-                <Form onSubmit={handleFormSubmit}>
-                    <Form.Group controlId="formBasicInput form-inline">
-                        <Form.Label style={{fontSize:'40px'}}>Hi Calvin! What can I help you with?</Form.Label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
-                    </Form.Group>
-                    <Button style={{backgroundColor:"black" ,border:'none', marginTop:'10px'}}type="submit">let's chat</Button>
-                </Form>
-            </div>
+          <div style={{ margin: '20px' }}>
+            <Form onSubmit={handleFormSubmit}>
+              <Form.Group controlId="formBasicInput form-inline">
+                <Form.Label style={{ fontSize: '40px' }}>Hi Calvin! What can I help you with?</Form.Label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="5" onChange={handleTextareaChange}></textarea>
+              </Form.Group>
+              <Button style={{ backgroundColor: "black", border: 'none', marginTop: '10px' }} type="submit">let's chat</Button>
+            </Form>
+          </div>
         </div>
-    </div>
+      </div>
     </Container>
   )
 }
