5537da50eff34588d96fcac32961ddaecae36d1c
Anakin Trotter <anakin.trotter@gmail.com>
2023-04-22 18:51:47 -0700

    dummy

--- convo/src/pages/api/chatquery.js
+++ convo/src/pages/api/chatquery.js
@@ -1,72 +1,27 @@
-// return a few questions delimited by ~
-import { Configuration, OpenAIApi } from "openai";
-
-const chatPrompt = (input) => {
-  return `
-I am going to give you a paragraph and I want you to create two questions about the contents of the paragraph.
-
-
-Output format should be as such:
-{question 1}
-{question 2}
-
-
-Begin First Example:
-
-
-Input:
-The auto-dependent transportation system has reached its limit in most major cities and megaregions. Commuting by car is among the least efficient of all our activities—not to mention among the least enjoyable, according to detailed research by the Nobel Prize–winning economist Daniel Kahneman and his colleagues. Though one might think that the economic crisis beginning in 2007 would have reduced traffic (high unemployment means fewer workers traveling to and from work), the opposite has been true. 
-
-
-Output: What are the negatives of commuting by car?
-
-
-End First Example
-
-
-Begin Second Example
-Input:
-The auto-dependent transportation system has reached its
-limit in most major cities and megaregions. Commuting by
-car is among the least efficient of all our activities—not to
-mention among the least enjoyable, according to detailed
-research by the Nobel Prize-winning economist Daniel
-Kahneman and his colleagues. Though one might think that
-the economic crisis beginning in 2007 would have reduced
-traffic (high unemployment means fewer workers traveling to
-and from work), the opposite has been true.
-
-
-Output: What research supports the claim that commuting by car is one of the least efficient activities?
-What is the overall cost of commuting to the US economy, and how does the Martin Prosperity Institute suggest reducing it?
-
-
-End First Example
-
-
-Below is the paragraph that I want you to make two questions about.
-${input}
-`
-}
-
 export default async function handler(req, res) {
-  const configuration = new Configuration({
-    apiKey: process.env.GPT_API_KEY,
-  });
+  const data = {
+    "id": "chatcmpl-abc123",
+    "object": "chat.completion",
+    "created": 1677858242,
+    "model": "gpt-3.5-turbo-0301",
+    "usage": {
+      "prompt_tokens": 13,
+      "completion_tokens": 7,
+      "total_tokens": 20
+    },
+    "choices": [
+      {
+        "message": {
+          "role": "assistant",
+          "content": `\n\nThis is a question ${uuidv4()}!~Here is another question! ${crypto.uuidv4()}`
+        },
+        "finish_reason": "stop",
+        "index": 0
+      }
+    ]
+  }
 
-  // console.log("PROMPT " + chatPrompt(req.body.paragraph))
+  const ans = data.choices[0].message.content.trim()
 
-  const openai = new OpenAIApi(configuration);
-
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: chatPrompt(req.body.paragraph),
-    max_tokens: 2048
-  });
-
-  console.log(completion.data.choices)
-
-  const ans = completion.data.choices[0].text.trim()
-
-  res.status(200).json({ questions: ans.split('\n') });
+  res.status(200).json({ questions: ans.split('~') });
 }
